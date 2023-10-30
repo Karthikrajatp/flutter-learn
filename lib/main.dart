214185898d42cfe567dfa27db6c56530a6a4cb7e
Karthikraja TP <tpkarthikraja@gmail.com>
2023-10-30 13:35:55 +0530

    gridview

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.blue[100],
-        body: ListView.builder(
-          itemCount: names.length,
-          itemBuilder: (context,index) => ListTile(
-            title: Text(names[index]),
-          ),
-          ),
+        body: GridView.builder(
+          itemCount: 64,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),
+           itemBuilder: (content,index)=>Container(
+            color: Colors.deepPurple,
+            margin: EdgeInsets.all(2),
+           ))
       ),
     );
   }
